5b3d2d6aee9d7400dec2999020d0d066096ef038
Manuel Martinez Peinado <kketzal@gmail.com>
2015-04-17 14:17:51 +0200

    MAking the functions....

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+##--------##---------##--------##---------##--------##---------##--------##
+## This function make a "special matrix" with methods to SET and GET
+## a "normal matrix" and his "inverted matrix". The matice##are stored 
+## Usage:
+##   ----- Create an empty "special matrix" ------
+##
+##          my_special_matrix <- makeCacheMatrix()
+##
+###------##--------##---------##--------##---------##--------##---------##
 
-## Write a short comment describing this function
+##   ----- Set a normal matrix "x" to my "special matrix" ------
+##          
+##          my_special_matrix$set_matrix(x)
+##
+##   ----- Get the normal matrix "x" from my "special matrix" ------
+##
+##          my_special_matrix$get_matrix()
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        inverted_matrix <- NULL
+        
+        set_matrix <- function(y) {
+                x <<- y
+                inverted_matrix <<- NULL
+        }
+        get_matrix <- function() x
+        set_inverted_matrix <- function(inv_mat) inverted_matrix <<- inv_mat
+        get_inverted_matrix <- function() inverted_matrix
+        
+        list(set_matrix = set_matrix, 
+             get_matrix = get_matrix,
+             set_inverted_matrix = set_inverted_matrix,
+             get_inverted_matrix = get_inverted_matrix)
 }
 
 
